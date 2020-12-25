0b79b2436d1d784db4b537e92996c6de52f8640a
Maksim_Mozhorov@epam.com <Maksim_Mozhorov@epam.com>
2020-12-25 19:12:47 +0300

    Added debug crib

---
+++ debug/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.get('/', (req, res) => {
+    debugger;
+    res.send('Hello World!')
+})
+
+app.listen( process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${ process.env.PORT }`)
+})
+
+
+/*
+Tap in console: PORT=3000 node inspect index.js 
+And open Chrome Dev Tools and click on NODE.JS logo
+
+*/
